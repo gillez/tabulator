b8e74cc095d7e6e98320c0cd0375976c784e10b9
Giles Ellis <giles@yoyotta.com>
2019-01-03 10:17:37 +0000

    Apply column cssClass to calculation row.
    - I'm going to submit this as a change request and may undo it if rejected.
    I can implement my widgets in a different way rather than using cssClass,
    which maybe better than another difference from the master code.

--- src/js/modules/calculation_colums.js
+++ src/js/modules/calculation_colums.js
@@ -239,6 +239,7 @@ ColumnCalcs.prototype.generateRow = function(pos, data){
 	row.generateCells = function(){
 
 		var cells = [];
+		var oldCssClass;
 
 		self.table.columnManager.columnsByIndex.forEach(function(column){
 
@@ -260,13 +261,24 @@ ColumnCalcs.prototype.generateRow = function(pos, data){
 					};
 				}
 
-				//generate cell and assign to correct column
+				if (column.definition.cssClass) {
+					oldCssClass = self.genColumn.definition.cssClass;
+					self.genColumn.definition.cssClass = self.genColumn.definition.cssClass ? 
+						[self.genColumn.definition.cssClass, column.definition.cssClass].join(" ") :
+						column.definition.cssClass;
+				}
+
+			//generate cell and assign to correct column
 				var cell = new Cell(self.genColumn, row);
 				cell.column = column;
 				cell.setWidth(column.width);
 
 				column.cells.push(cell);
 				cells.push(cell);
+
+				if (column.definition.cssClass) {
+					self.genColumn.definition.cssClass = oldCssClass;
+				}
 			}
 		});
 
